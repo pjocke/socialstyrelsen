32f36a8bf12bbc9e25b4e720e636c8004a0d8fb1
Joakim Bomelin <joakim@bomelin.com>
2017-12-19 15:36:20 +0100

    Don't stop at first vowel if word ends with a vowel.

--- split.go
+++ split.go
@@ -6,28 +6,38 @@ import (
 	"errors"
 )
 
-func isVowel(c string) bool {
-	return strings.ContainsAny(strings.ToLower(c), "aeiouyåäö")
-}
-
 func getEnding(s string) (error, string) {
 	word := []rune(s)
 
 	l := len(word)
+
+	levels := 1
+	if strings.ContainsAny(strings.ToLower(string(word[l-1])),  "aeiouyåäö") {
+		levels = 2
+	}
+
+	level := 0
 	for i := l; i > 0; i-- {
-		if isVowel(string(word[i-1])){
+		if strings.ContainsAny(strings.ToLower(string(word[i-1])),  "aeiouyåäö") {
+			level++
+		}
+		if level == levels {
 			return nil, string(word[i-1:l])
 		}
 	}
+
 	return errors.New("fel fel fel"), ""
 }
 
 func main() {
-	//words := []string{"ambulans", "pomerans", "hästsvans", "resistans"}
-	err, end := getEnding("resistans")
-	if err != nil {
-		fmt.Printf("ERR: %s\n", err)
-	}
+	words := []string{"ambulans", "pomerans", "hästsvans", "resistans", "kanelbulle", "höskulle", "bildrulle"}
 
-	fmt.Printf("%s\n", end)
+	for _ ,w := range(words) {
+		err, end := getEnding(w)
+		if err != nil {
+			fmt.Printf("ERR: %s\n", err)
+		}
+
+		fmt.Printf("%s\t\t%s\n", w, end)
+	}
 }
